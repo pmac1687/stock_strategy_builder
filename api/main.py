8b80f5f5790ac2117127501e66257364010712c5
pmac1687 <pmac1687@outlook.com>
2021-05-14 10:05:40 -0700

    main

---
+++ api/main.py
@@ -0,0 +1,21 @@
+import psycopg2
+
+
+
+"""conn = psycopg2.connect(database="postgres", user="pat", password="1687", host="54.163.101.217", port="5432")
+
+cur = conn.cursor()
+
+cur.execute(f"select * from master_ticker_list")
+
+#print(cur.fetchone())
+
+
+
+
+conn.commit()
+print("Records created successfully")
+conn.close()"""
+
+if __name__=='__main__':
+    print('hello')
--- api/test.py
+++ api/test.py
@@ -1,4 +1,18 @@
-import keys
+import psycopg2
 
-print(keys.name)
 
+
+conn = psycopg2.connect(database="postgres", user="pat", password="1687", host="54.163.101.217", port="5432")
+
+cur = conn.cursor()
+
+cur.execute(f"select * from master_ticker_list")
+
+#print(cur.fetchone())
+
+
+
+
+conn.commit()
+print("Records created successfully")
+conn.close()
